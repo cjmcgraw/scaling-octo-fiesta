2cb5d9dc0698d8313e1c3a3e051f2e8a7d4700ad
carlm <c@rlmcgraw.io>
2021-10-22 14:58:17 -0700

    updates

--- packet_reader/main.go
+++ packet_reader/main.go
@@ -17,33 +17,10 @@ func main() {
 	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packetSource.Packets() {
 		fmt.Printf("inbound packet...\n")
-		fmt.Println(packet.Dump())
-
-		if errorLayer := packet.ErrorLayer(); errorLayer != nil {
-			fmt.Printf("  errors decoding layer: %s\n", errorLayer)
+		fmt.Printf("truncated: %t\n", packet.Metadata().Truncated)
+		for i, layer := range packet.Layers() {
+			fmt.Printf("layer #%d: %s\n", i, layer.LayerType())
 		}
-		fmt.Println("  Link Layer:")
-		if linkLayer := packet.LinkLayer(); linkLayer != nil {
-			flow := linkLayer.LinkFlow()
-			fmt.Printf("    type: %s\n", linkLayer.LayerType())
-			fmt.Printf("    format: %s\n", flow.EndpointType())
-			fmt.Printf("    source MAC addr: %s\n", flow.Src())
-			fmt.Printf("    dest MAC addr: %s\n", flow.Dst())
-		} else {
-			fmt.Printf("  Unknown link packet!")
-		}
-
-		fmt.Println("  Network Layer:")
-		if networkLayer := packet.NetworkLayer(); networkLayer != nil {
-			flow := networkLayer.NetworkFlow()
-			fmt.Printf("    type: %s\n", networkLayer.LayerType())
-			fmt.Printf("    format: %s\n", flow.EndpointType())
-			fmt.Printf("    source ip: %s\n", flow.Src())
-			fmt.Printf("    dest ip: %s\n", flow.Dst())
-		} else {
-			fmt.Println("  Unknown network packet!")
-		}
-
 		fmt.Println("")
 	}
 }
