ce91034170c9657cece769aa41003c82f4fbeaa5
carlm <c@rlmcgraw.io>
2021-10-25 11:19:31 -0700

    updated to determine the device automatically. Right now that just means it has traffic. Later we will filter for our exact packet we are seeking

--- packet_reader/go.mod
+++ packet_reader/go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/google/gopacket v1.1.19
+	golang.org/x/net v0.0.0-20190620200207-3b0461eec859
 	rsc.io/quote v1.5.2
 )
--- packet_reader/main.go
+++ packet_reader/main.go
@@ -2,34 +2,82 @@ package main
 
 import (
 	"fmt"
-
+	"log"
+	"time"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/pcap"
 )
 
-var ()
+var (
+	maxPacketSize int = 65535
+)
+
+func getBestGuessDeviceName() (string, error) {
+	log.Println("scanning available devices that are transmitting...")
+	// Find all devices
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	checkDeviceForValidPackets := func(deviceName string) (string, error) {
+		log.Printf("checking device: %s\n", deviceName)
+		handle, err := pcap.OpenLive(deviceName, 1, true, 1 * time.Second)
+		if err != nil {
+			return "", err
+		} 
+		packetSrc := gopacket.NewPacketSource(handle, handle.LinkType())
+
+		validPacketCount := 0
+		for i:= 0; i < 30; i++ {
+			packet, err := packetSrc.NextPacket()
+			if err == nil && packet != nil {
+				validPacketCount++
+			}
+		}
+		handle.Close()
+		if validPacketCount < 20 {
+			log.Printf("failed to find active/valid device: %s\n", deviceName)
+			return "errors", fmt.Errorf("failed to find valid traffic on device: %s", deviceName)
+		}
+
+		log.Printf("found active/valid device: %s\n", deviceName)
+		return deviceName, nil
+	}
+
+	// Print device information
+	log.Println("Devices found:")
+
+	for _, device := range(devices) {
+		log.Printf(" - %s\n", device.Name)
+	}
+
+	for _, device := range devices {
+		if device.Name != "any" {
+			result, err := checkDeviceForValidPackets(device.Name)
+			if err == nil {
+				return result, err
+			}
+		}
+	}
+	return "failed", err
+}
 
 func main() {
-	devices, err := pcap.FindAllDevs()
-	if err != nil { panic(err)}
-
-	fmt.Println("available devices: ")
-	for _, device := range devices {
-		fmt.Printf("  %s\n", device.Name)
-	}
-	fmt.Println("")
-
-	handle, err := pcap.OpenLive("enp71s0", 65335, true, pcap.BlockForever)
+	deviceName, err := getBestGuessDeviceName()
 	if err != nil {
 		panic(err)
 	}
-	packetSource := gopacket.NewPacketSource(handle, handle.LinkType())
-	for packet := range packetSource.Packets() {
-		fmt.Printf("inbound packet...\n")
-		fmt.Printf("truncated: %t\n", packet.Metadata().Truncated)
-		for i, layer := range packet.Layers() {
-			fmt.Printf("layer #%d: %s\n", i, layer.LayerType())
-		}
+
+	log.Printf("using device: %s\n", deviceName)
+	handle, err := pcap.OpenLive(deviceName, 65535, true, 100 * time.Millisecond)
+	if err != nil {
+		panic(err)
+	}
+
+	packetSrc := gopacket.NewPacketSource(handle, handle.LinkType())
+	for packet := range packetSrc.Packets() {
+		fmt.Println(packet)
 		fmt.Println("")
 	}
 }
